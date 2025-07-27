832ed94d3048e2c576647d3cd33239a1e07d3356
Mohamed Shibl <mohamed@boompop.com>
2025-07-27 13:53:44 +0300

    trying out changes

--- app/components/allposts.tsx
+++ app/components/allposts.tsx
@@ -1,18 +1,18 @@
 import { PostPreview } from "./post-preview";
 import { PostMetaFragment } from "./hero-post";
 
-export function MoreStories({
+export function AllPosts({
   allPosts,
-  title,
+  // title,
 }: {
   allPosts: PostMetaFragment[];
-  title: React.ReactNode;
+  // title: React.ReactNode;
 }) {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+      {/* <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
         {title}
-      </h2>
+      </h2> */}
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {allPosts.map((post) => {
           return <PostPreview key={post._id} {...post} />;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Pump } from "basehub/react-pump";
 import { Intro } from "./components/intro";
 import { HeroPost, PostMetaFragment } from "./components/hero-post";
 import { MoreStories } from "./components/more-stories";
+import { AllPosts } from "./components/allposts";
 
 export const dynamic = "force-static";
 
@@ -23,15 +24,17 @@ export default async function Page() {
       {async ([{ blog }]) => {
         "use server";
 
-        const heroPost = blog.posts.items[0];
-        const morePosts = blog.posts.items.slice(1);
+        // const heroPost = blog.posts.items[0];
+        // const morePosts = blog.posts.items.slice(1);
+        const allPosts = blog.posts.items;
 
         return (
           <main>
             <section className="container mx-auto px-5">
               <Intro />
-              {heroPost && <HeroPost {...heroPost} />}
-              <MoreStories morePosts={morePosts} title={blog.morePosts} />
+              {/* {heroPost && <HeroPost {...heroPost} />} */}
+              {/* <MoreStories morePosts={morePosts} title={blog.morePosts} /> */}
+              <AllPosts allPosts={allPosts} />
             </section>
           </main>
         );
